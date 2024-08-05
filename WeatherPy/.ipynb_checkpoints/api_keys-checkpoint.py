524d61f1c0da17825e06ee469803827f7e3709da
ysdarji <darjiyashesh@yahoo.com>
2024-08-05 15:58:29 -0400

    added output file

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
 weather_api_key = "YOUR KEY HERE"
-weather_api_key = "d17d9eee233bf8a57cd84f2afeddcdc8"
+
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
-geoapify_key = "25ce59a3077a489b8805937f2accb94d"
